a58ba668253c7c6789b7ad78a5896f261a04ba5f
LaviRastogi <1993lavirastogi@gmail.com>
2025-01-08 17:50:50 +0530

    quiz

--- constants/htmlQuizDetails.js
+++ constants/htmlQuizDetails.js
@@ -9,7 +9,7 @@ export let HTMLQuizDetails = {
         id: "quizFree1",
         totalQuestions: 15,
         completedQuestions: 0,
-        isCompleted: true,
+        isCompleted: false,
         active: true,
         duration: "15 minutes",
       },
