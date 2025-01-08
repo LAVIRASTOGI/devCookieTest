b291d4e08cf5672f4bb6147eb36ddd3eaa6d2a75
LaviRastogi <1993lavirastogi@gmail.com>
2025-01-08 17:44:41 +0530

    loading

--- app/(home)/quizzes/loadingQuiz.jsx
+++ app/(home)/quizzes/loadingQuiz.jsx
@@ -1,6 +1,6 @@
 export default function LoadingQuiz() {
   return (
-    <div className="fixed inset-0 bg-gray-500/50  h-[100vh] flex items-center justify-center">
+    <div className="fixed inset-0 bg-gray-500/50  h-[100vh] flex items-center justify-center z-50">
       <div className="bg-white p-8 rounded-lg shadow-lg space-y-4 text-center">
         <div className="w-16 h-16 mx-auto">
           <div className="loading loading-spinner loading-lg text-primary"></div>
